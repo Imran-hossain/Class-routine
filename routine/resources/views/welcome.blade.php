0fa3584e572aae26e9e8fb26297985e52e9b5d80
Imran-hossain <imran75442@gmail.com>
2019-10-10 13:17:22 +0900

    Just a test react run, might revert

--- routine/resources/views/welcome.blade.php
+++ routine/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    <div id="example">
+                    <div id="app">
                         
                     </div>
                 </div>
