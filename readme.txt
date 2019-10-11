2eba5eb46ea93d6a361119fced7d4e02061a24d4
Imran-hossain <imran75442@gmail.com>
2019-10-11 15:53:58 +0900

    Online mongo DB has been connected

--- readme.txt
+++ readme.txt
@@ -1,5 +1,11 @@
 #HELPS
 
+#Create laravel with mongo db
+https://github.com/jenssegers/laravel-mongodb
+
+#Passport for laravel with mongoDB
+https://medium.com/@bestafiko/laravel-jenssegers-mongodb-supports-laravel-passport-by-another-dependency-e2cfd6981809
+
 
 #Create laravel-auth-routes with react
 https://medium.com/@panjeh/laravel-auth-routes-email-verification-reset-password-authentication-registration-routes-fb82b3337150
--- routine/config/database.php
+++ routine/config/database.php
@@ -44,15 +44,9 @@ return [
         ],
         
         'mongodb' => [
-            'driver'   => 'mongodb',
-            'host'     => env('DB_HOST', 'localhost'),
-            'port'     => env('DB_PORT', 27017),
-            'database' => env('DB_DATABASE'),
-            'username' => env('DB_USERNAME'),
-            'password' => env('DB_PASSWORD'),
-            'options'  => [
-                'database' => 'admin' // sets the authentication database required by mongo 3
-            ]
+            'driver' => 'mongodb',
+            'dsn'=>'mongodb+srv://bdhuebciib:HfxRtfloj2b9dxRz@hrcluster-rxhry.azure.mongodb.net/test?retryWrites=true&w=majority',
+            'database' => 'class_routine',
         ],
 
         'mysql' => [
