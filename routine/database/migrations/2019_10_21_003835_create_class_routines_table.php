e7a2c60f288d3aae3ac8bc6c2d40c9ee3bedd145
dilshadjahanrupaa <40635967+dilshadjahanrupaa@users.noreply.github.com>
2019-10-21 10:04:47 +0900

    Database_table

---
+++ routine/database/migrations/2019_10_21_003835_create_class_routines_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClassRoutinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('class_routine', function (Blueprint $table) {
+      
+
+            $table->bigIncrements('id');
+            $table->string('time');
+            $table->string('teacher_name');
+            $table->string('group_name');
+            $table->rememberToken();
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('class_routines');
+    }
+}
---
+++ routine/database/migrations/2019_10_21_003848_create_locations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('location', function (Blueprint $table) {
+            
+
+            $table->bigIncrements('id');
+            $table->string('building_name');
+            $table->string('x');
+             $table->string('y');
+            $table->string('group_name');
+            $table->rememberToken();
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
